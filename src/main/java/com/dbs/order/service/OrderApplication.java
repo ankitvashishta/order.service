df7087398c05e03536b02d006f30eb5933ebaa96
ankitvashishta@gmail.com <ankitvashishta@gmail.com>
2020-10-06 10:41:02 +0530

    Organizing dependencies.

--- src/main/java/com/dbs/order/service/OrderApplication.java
+++ src/main/java/com/dbs/order/service/OrderApplication.java
@@ -2,7 +2,6 @@ package com.dbs.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -10,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableHystrix
-@EnableCircuitBreaker
 @EnableFeignClients
 @EnableSwagger2
 public class OrderApplication {
